4d443c2ae231d4267447d13064d5d5c2a0a4cc17
Freddyede <patouillardfranck3@gmail.com>
2020-02-21 20:14:05 +0100

    edit front

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -23,7 +23,7 @@ describe('AppComponent', () => {
   it(`should have as title 'front'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('front');
+    expect(app.title.name).toEqual('front app');
   });
 
   it('should render title', () => {
